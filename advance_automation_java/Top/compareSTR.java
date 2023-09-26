e961201e099f5e52df030f66b5c862706fd0cdeb
addy2047 <116457686+addy2047@users.noreply.github.com>
2023-09-26 15:55:37 +0530

    Add files via upload

---
+++ advance_automation_java/Top/compareSTR.java
@@ -0,0 +1,19 @@
+package TOP;
+
+public class compareSTR {
+
+	
+	
+	public static void main(String[] args) {
+		String s1,s2,s3,s4;
+		s1 ="hello";
+		s2 ="hello";
+		s3 ="how are U";
+		s4 ="Fine";
+		System.out.println(s1==s2);
+		System.out.println(s1==s3);
+		System.out.println(s2==s3);
+		System.out.println(s4==s1);
+		
+	}
+}
