4108352a1da615ccfecba9d4da816e7dd6cbe7c9
addy2047 <116457686+addy2047@users.noreply.github.com>
2023-09-26 15:48:55 +0530

    Add files via upload

---
+++ advance_automation_java/Top/compareSTRequals.java
@@ -0,0 +1,15 @@
+package TOP;
+
+public class compareSTRequals {
+
+	
+	
+	public static void main(String[] args) {
+		String s1,s2;
+		s1 ="hello";
+		s2 ="hell";
+		
+		System.out.println(s1.equals(s2));
+		
+	}
+}
